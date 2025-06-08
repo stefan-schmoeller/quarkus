e43da99d2fec3e7b285290aee600ff8e6337845a
Stefan Schmöller <stefan.schmoeller@googlemail.com>
2025-06-09 00:36:52 +0200

    FIx error with injection of config property in test

--- integration-tests/test-extension/tests/src/test/java/io/quarkus/it/extension/EndTestWithAttribute.java
+++ integration-tests/test-extension/tests/src/test/java/io/quarkus/it/extension/EndTestWithAttribute.java
@@ -1,26 +1,19 @@
 package io.quarkus.it.extension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import jakarta.inject.Inject;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.junit.QuarkusTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @CustomResourceWithAttribute(value = "bar")
 @QuarkusTest
 public class EndTestWithAttribute {
 
-    @Inject
-    @ConfigProperty(name = "attributeValue")
-    String attributeValue;
-
     @Test
     public void test1() {
-        assertEquals("bar", attributeValue);
+        assertEquals("bar", ConfigProvider.getConfig().getValue("attributeValue", String.class));
         assertTrue(Counter.endCounter.get() <= 1);
     }
 
--- integration-tests/test-extension/tests/src/test/java/io/quarkus/it/extension/StartTestWithAttribute.java
+++ integration-tests/test-extension/tests/src/test/java/io/quarkus/it/extension/StartTestWithAttribute.java
@@ -1,26 +1,19 @@
 package io.quarkus.it.extension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import jakarta.inject.Inject;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.junit.QuarkusTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @CustomResourceWithAttribute(value = "foo")
 @QuarkusTest
 public class StartTestWithAttribute {
 
-    @Inject
-    @ConfigProperty(name = "attributeValue")
-    String attributeValue;
-
     @Test
     public void test1() {
-        assertEquals("foo", attributeValue);
+        assertEquals("bar", ConfigProvider.getConfig().getValue("attributeValue", String.class));
         assertTrue(Counter.startCounter.get() <= 1);
     }
 
